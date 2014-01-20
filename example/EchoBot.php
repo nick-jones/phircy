cf076767d5f96c4b0f961a4457088f6965dfb925
Nicholas Jones <me@nicholasjon.es>
2014-01-20 13:44:23 +0000

    Example IRC EchoBot, for testing

---
+++ example/EchoBot.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$listeners = [
+    'irc.privmsg' => function(\Phircy\Event\TargetedIrcEvent $event) {
+        $params = $event->getParams();
+
+        if (!preg_match('/^say (.+)/', $params['text'], $matches)) {
+            return;
+        }
+
+        $event->getConnection()
+            ->transport
+            ->writePrivmsg($params['receivers'], $matches[1]);
+    }
+];
+
+$phircy = new \Phircy\Application([
+    'networks' => [
+        [
+            'name' => 'EFnet',
+            'nick' => 'monkey_eh',
+            'username' => 'monkey',
+            'realname' => 'monkey',
+            'servers' => 'irc.efnet.org',
+            'channels' => [
+                '#monkey_casita'
+            ]
+        ]
+    ],
+    'listeners' => $listeners
+]);
+
+$phircy->execute();
