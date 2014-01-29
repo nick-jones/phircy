84f1829905eb772fc2efd7038469b5cf4996383b
Nicholas Jones <me@nicholasjon.es>
2014-01-29 15:12:59 +0000

    Adding Matcher interface, to be used by plugins for matching against message strings

---
+++ lib/Phircy/Matcher/Matcher.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Phircy\Matcher;
+
+/**
+ * Matcher implementations should indicate whether supplied messages match their expectations.
+ *
+ * @package Phircy\Matcher
+ */
+interface Matcher {
+    /**
+     * Indicates whether the supplied message matches the implementations expectations.
+     *
+     * @param string $message
+     * @return bool
+     */
+    public function matches($message);
+}
